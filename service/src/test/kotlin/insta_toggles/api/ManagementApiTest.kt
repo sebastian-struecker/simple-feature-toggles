e75be4b284dce7f309d7aa924631bcc48a4f4cf9
Sebastian Strücker <sebastian.struecker@mhp.com>
2024-07-18 12:12:03 +0200

    More tests

--- service/src/test/kotlin/insta_toggles/api/ManagementApiTest.kt
+++ service/src/test/kotlin/insta_toggles/api/ManagementApiTest.kt
@@ -3,14 +3,19 @@ package insta_toggles.api
 import insta_toggles.Feature
 import insta_toggles.FeaturePanacheRepository
 import insta_toggles.FeatureService
+import insta_toggles.api.models.CreateFeatureRequest
+import insta_toggles.api.models.PartialFeatureUpdateRequest
 import io.quarkus.test.junit.QuarkusMock
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.security.TestSecurity
 import io.quarkus.test.vertx.RunOnVertxContext
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import io.restassured.response.Response
 import io.smallrye.mutiny.Uni
 import jakarta.inject.Inject
 import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
@@ -20,17 +25,25 @@ import org.mockito.Mockito
 @RunOnVertxContext
 class ManagementApiTest {
 
-    @Inject lateinit var serviceMock: FeatureService
+    @Inject
+    lateinit var serviceMock: FeatureService
 
-    @Inject lateinit var repositoryMock: FeaturePanacheRepository
+    @Inject
+    lateinit var repositoryMock: FeaturePanacheRepository
 
     @BeforeEach
     fun setupMocks() {
-        serviceMock = Mockito.mock(FeatureService::class.java)
-        QuarkusMock.installMockForType(serviceMock, FeatureService::class.java)
-
         repositoryMock = Mockito.mock(FeaturePanacheRepository::class.java)
         QuarkusMock.installMockForType(repositoryMock, FeaturePanacheRepository::class.java)
+
+        serviceMock = Mockito.mock(FeatureService::class.java)
+        QuarkusMock.installMockForType(serviceMock, FeatureService::class.java)
+    }
+
+    @AfterEach
+    fun resetMocks() {
+        Mockito.reset(repositoryMock)
+        Mockito.reset(serviceMock)
     }
 
     companion object {
@@ -38,53 +51,141 @@ class ManagementApiTest {
     }
 
     @Test
+    fun unauthorized_getAll_test() {
+        getAllRequest().then().statusCode(401)
+    }
+
+    @Test
     @TestSecurity(user = "admin", roles = [DefaultRoles.ADMIN])
     fun admin_getAll_test() {
-        getAll_test()
+        getAllRequest().then().statusCode(200)
     }
 
     @Test
     @TestSecurity(user = "viewer", roles = [DefaultRoles.VIEWER])
     fun viewer_getAll_test() {
-        getAll_test()
+        getAllRequest().then().statusCode(200)
     }
 
     @Test
     @TestSecurity(user = "release_manager", roles = [DefaultRoles.RELEASE_MANAGER])
     fun release_manager_getAll_test() {
-        getAll_test()
+        getAllRequest().then().statusCode(200)
+    }
+
+    @Test
+    fun unauthorized_getById_test() {
+        getById(1).then().statusCode(401)
     }
 
     @Test
     @TestSecurity(user = "admin", roles = [DefaultRoles.ADMIN])
     fun admin_getById_test() {
-        getById_test()
+        val feature = feature()
+        Mockito.`when`(repositoryMock.getById(1)).thenReturn(
+            Uni.createFrom().item(feature)
+        )
+        getById(1).then().statusCode(200).body("name", `is`(feature.name))
+            .body("description", `is`(feature.description))
     }
 
     @Test
     @TestSecurity(user = "viewer", roles = [DefaultRoles.VIEWER])
     fun viewer_getById_test() {
-        getById_test()
+        val feature = feature()
+        Mockito.`when`(repositoryMock.getById(1)).thenReturn(
+            Uni.createFrom().item(feature)
+        )
+        getById(1).then().statusCode(200).body("name", `is`(feature.name))
+            .body("description", `is`(feature.description))
     }
 
     @Test
     @TestSecurity(user = "release_manager", roles = [DefaultRoles.RELEASE_MANAGER])
     fun release_manager_getById_test() {
-        getById_test()
-    }
-
-    private fun getAll_test() {
-        given().`when`().get(managementUrl).then().statusCode(200)
-    }
-
-    private fun getById_test() {
         val feature = feature()
         Mockito.`when`(repositoryMock.getById(1)).thenReturn(
             Uni.createFrom().item(feature)
         )
-        given().`when`().get("$managementUrl/1").then().statusCode(200).body("name", `is`(feature.name))
+        getById(1).then().statusCode(200).body("name", `is`(feature.name))
             .body("description", `is`(feature.description))
     }
+
+    @Test
+    fun unauthorized_create_test() {
+        val request = CreateFeatureRequest("name", "description")
+        createRequest(request).then().statusCode(401)
+    }
+
+    @Test
+    @TestSecurity(user = "admin", roles = [DefaultRoles.ADMIN])
+    fun admin_create_test() {
+        val feature = feature()
+        Mockito.`when`(serviceMock.create(feature.name, feature.description)).thenReturn(
+            Uni.createFrom().item(feature)
+        )
+        val request = CreateFeatureRequest(feature.name, feature.description)
+        createRequest(request).then().statusCode(200).body("name", `is`(request.name))
+            .body("description", `is`(request.description))
+    }
+
+    @Test
+    @TestSecurity(user = "viewer", roles = [DefaultRoles.VIEWER])
+    fun viewer_create_test() {
+        val request = CreateFeatureRequest("name", "description")
+        createRequest(request).then().statusCode(403)
+    }
+
+    @Test
+    fun unauthorized_partialUpdate_test() {
+        val request = PartialFeatureUpdateRequest(null, null, isActive = true)
+        partialUpdateRequest(1, request).then().statusCode(401)
+    }
+
+// TODO: Fix me
+//    @Test
+//    @TestSecurity(user = "admin", roles = [DefaultRoles.ADMIN])
+//    fun admin_partialUpdate_test() {
+//        val feature = feature()
+//        val request = PartialFeatureUpdateRequest("updated", "updated", isActive = true)
+//        Mockito.`when`(
+//            serviceMock.update(
+//                feature.id, request
+//            )
+//        ).thenReturn(
+//            Uni.createFrom().item(feature.apply {
+//                name = "updated"
+//                description = "updated"
+//                isActive = true
+//            })
+//        )
+//        partialUpdateRequest(1, request).then().statusCode(200).body("name", `is`(request.name))
+//            .body("description", `is`(request.description))
+//    }
+
+    @Test
+    @TestSecurity(user = "viewer", roles = [DefaultRoles.VIEWER])
+    fun viewer_partialUpdate_test() {
+        val request = PartialFeatureUpdateRequest(null, null, isActive = true)
+        partialUpdateRequest(1, request).then().statusCode(403)
+    }
+
+    @Test
+    @TestSecurity(user = "release_manager", roles = [DefaultRoles.RELEASE_MANAGER])
+    fun release_manager_partialUpdate_test() {
+        val request = PartialFeatureUpdateRequest(null, null, isActive = true)
+        partialUpdateRequest(1, request).then().statusCode(403)
+    }
+
+    private fun getAllRequest(): Response = given().`when`().get(managementUrl)
+
+    private fun getById(id: Long): Response = given().`when`().get("$managementUrl/$id")
+
+    private fun createRequest(request: CreateFeatureRequest): Response =
+        given().`when`().body(request).contentType(ContentType.JSON).post(managementUrl)
+
+    private fun partialUpdateRequest(id: Long, request: PartialFeatureUpdateRequest): Response =
+        given().`when`().body(request).contentType(ContentType.JSON).patch("$managementUrl/$id")
 
     private fun feature() = Feature(1L, "name", "description", false)
 
