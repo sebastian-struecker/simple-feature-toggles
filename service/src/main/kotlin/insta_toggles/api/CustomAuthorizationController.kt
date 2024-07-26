5fa9dcbbd10b2f7b5cce8a342ced92a5aab70bbc
Sebastian Strücker <sebastian.struecker@mhp.com>
2024-07-26 16:02:10 +0200

    Fix tests

--- service/src/main/kotlin/insta_toggles/api/CustomAuthorizationController.kt
+++ service/src/main/kotlin/insta_toggles/api/CustomAuthorizationController.kt
@@ -3,11 +3,9 @@ package insta_toggles.api
 import io.quarkus.security.spi.runtime.AuthorizationController
 import jakarta.annotation.Priority
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.enterprise.inject.Alternative
 import jakarta.interceptor.Interceptor
 import org.eclipse.microprofile.config.inject.ConfigProperty
 
-@Alternative
 @Priority(Interceptor.Priority.LIBRARY_AFTER)
 @ApplicationScoped
 class CustomAuthorizationController(
