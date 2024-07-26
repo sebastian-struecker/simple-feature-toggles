403775a305196d5abbcb63318fe6ce9a088c62ee
Sebastian Strücker <sebastian.struecker@mhp.com>
2024-07-26 17:07:34 +0200

    Fix authorization controller

--- service/src/main/kotlin/insta_toggles/api/CustomAuthorizationController.kt
+++ service/src/main/kotlin/insta_toggles/api/CustomAuthorizationController.kt
@@ -1,13 +1,17 @@
 package insta_toggles.api
 
+import io.quarkus.arc.profile.UnlessBuildProfile
 import io.quarkus.security.spi.runtime.AuthorizationController
 import jakarta.annotation.Priority
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Alternative
 import jakarta.interceptor.Interceptor
 import org.eclipse.microprofile.config.inject.ConfigProperty
 
+@Alternative
 @Priority(Interceptor.Priority.LIBRARY_AFTER)
 @ApplicationScoped
+@UnlessBuildProfile("test")
 class CustomAuthorizationController(
     @ConfigProperty(name = "api.authorization.enable", defaultValue = "true")
     val authorizationEnabled: Boolean
