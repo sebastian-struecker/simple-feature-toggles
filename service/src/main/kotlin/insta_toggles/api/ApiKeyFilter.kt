e82a73fb0cf1643af262ec0c7a6804497f4acf66
Sebastian Strücker <sebastian.struecker@mhp.com>
2024-07-26 15:56:56 +0200

    Add authorization deactivate option

--- service/src/main/kotlin/insta_toggles/api/ApiKeyFilter.kt
+++ service/src/main/kotlin/insta_toggles/api/ApiKeyFilter.kt
@@ -1,13 +1,17 @@
 package insta_toggles.api
 
+import jakarta.enterprise.context.ApplicationScoped
 import jakarta.ws.rs.NotAuthorizedException
 import jakarta.ws.rs.container.ContainerRequestContext
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.jboss.resteasy.reactive.server.ServerRequestFilter
 
+@ApplicationScoped
 class ApiKeyFilter(
-    @ConfigProperty(name = "api-keys")
-    val apiKeys: List<String>
+    @ConfigProperty(name = "api.authorization.keys")
+    val apiKeys: List<String>,
+    @ConfigProperty(name = "api.authorization.enable", defaultValue = "true")
+    val authorizationEnabled: Boolean
 ) {
 
     companion object {
@@ -16,6 +20,7 @@ class ApiKeyFilter(
 
     @ServerRequestFilter(preMatching = true)
     fun filter(requestContext: ContainerRequestContext) {
+        if (!authorizationEnabled) return
         val path = requestContext.uriInfo.path
         if (isApiKeySecurePath(path)) {
             val apiKeyHeader = requestContext.getHeaderString(apiKeyHeaderName)
